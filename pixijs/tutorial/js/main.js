f3709d35ccbe3a400624bc03e640f36ad4da3f08
kraeuschen <infoqkraeuschen.de>
2014-03-15 16:41:43 +0100

    pixijs tutorial

---
+++ pixijs/tutorial/js/main.js
@@ -0,0 +1,34 @@
+var WIDTH = 400;
+var HEIGHT = 300;
+
+var stage = new PIXI.Stage(0xEEFFFF);
+
+// let pixi choose WebGL or canvas
+var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT);
+
+// attach render to page
+document.body.appendChild(renderer.view);
+
+var texture = PIXI.Texture.fromImage('assets/leaf.png');
+var leaf = new PIXI.Sprite(texture);
+
+// rotate around center
+leaf.anchor.x = 0.5;
+leaf.anchor.y = 0.5;
+
+// center in stage
+leaf.position.x = WIDTH / 2;
+leaf.position.y = HEIGHT / 2;
+
+// place it on the stage for rendering
+stage.addChild(leaf);
+
+console.log(leaf);
+
+function gameLoop() {
+  requestAnimFrame(gameLoop);
+  leaf.rotation -= 0.02;
+  renderer.render(stage);
+}
+
+requestAnimFrame(gameLoop);
